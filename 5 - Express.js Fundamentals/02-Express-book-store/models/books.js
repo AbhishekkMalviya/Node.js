20d4b8cbc156e11ef2d8996dfcb6b267dd4bf2f7
Abhishek Malviya <abhishekmalviya997@gmail.com>
2025-10-29 12:55:14 +0530

    rearrange in MVC architecture

--- 5 - Express.js Fundamentals/02-Express-book-store/models/books.js
+++ 5 - Express.js Fundamentals/02-Express-book-store/models/books.js
@@ -1,3 +1,4 @@
+//In memory db (Models)
 exports.BOOKS = [
     { id: 1, title: 'Book 1', author: 'Author1' },
     { id: 2, title: 'Book 2', author: 'Author2' },
--- 5 - Express.js Fundamentals/02-Express-book-store/routes/book.routes.js
+++ 5 - Express.js Fundamentals/02-Express-book-store/routes/book.routes.js
@@ -1,53 +1,14 @@
 const express = require('express');
-const {BOOKS} = require('../db/books')
+const controller = require('../controllers/book.controller')
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json(BOOKS);
-});
+router.get('/', controller.getAllBooks);
 
-router.get('/:id', function (req, res) {
-    const id = parseInt(req.params.id);
-    const book = BOOKS.find((e) => e.id == id);
+router.get('/:id', controller.getBookById)
 
-    if (isNaN(id)) return res.status(400).json({ error: "id must be number" }
-    )
-    if (!book) return res.status(404).end(`book with id ${id} does not exists`)
+router.post('/', controller.createBook)
 
-    return res.json(book)
-})
-
-router.post('/', (req, res) => {
-    const { title, author } = req.body;
-
-    if (!title || title === '')
-        return res.status(400).json({ error: `Title is required` });
-    if (!author || author === '')
-        return res.status(400).json({ error: `Author is required` });
-
-    const id = BOOKS.length + 1;
-
-    const book = { id, title, author }
-    BOOKS.push(book)
-
-    return res.status(201).json({ message: `Book is created successfully` });
-})
-
-router.delete('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-
-    if (isNaN(id)) return res.status(404).json({ error: "id must be number" });
-
-    const indexToDelete = BOOKS.findIndex((e) => e.id === id)
-
-    if (indexToDelete < 0)
-        return res.status(404)
-            .json({ error: `Book with id ${id} does not exist.` });
-
-    BOOKS.splice(indexToDelete, 1);
-
-    return res.status(200).json({ message: `book with id ${id} deleted successfully` });
-})
+router.delete('/:id', controller.deleteBookById)
 
 
 module.exports = router;
